a0ddb397b3c68768235558c34666e42dae1a0f70
imhrishikesh7 <123190220+imhrishikesh7@users.noreply.github.com>
2023-09-07 07:43:05 -1100

    Add files via upload

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
 
 export const connectDB = async() => {
-
-    const {connection} = await mongoose.connect(process.env.MONGO_URI);
+    try{
+    const {connection} = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
     console.log(`Mongo DB is Connected with ${connection.host}`);
-}
+    }catch (error) {
+        console.error("Mongo DB connection error:", error);
+        // You can handle the error further, such as logging or terminating the application.
+      }
+};
