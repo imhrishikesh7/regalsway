cd77b6ac9fbd02e6887364875278e5903b7a8e66
imhrishikesh7 <123190220+imhrishikesh7@users.noreply.github.com>
2023-09-07 20:29:45 -1100

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,6 @@ export const connectDB = async() => {
     try{
     const {connection} = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
     });
     console.log(`Mongo DB is Connected with ${connection.host}`);
     }catch (error) {
