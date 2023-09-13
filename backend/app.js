729727f9c6ff731d7265ac4b073b7757f60da6f7
imhrishikesh7 <123190220+imhrishikesh7@users.noreply.github.com>
2023-09-13 06:02:43 -1100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import paymentRoute from "./routes/paymentRoutes.js";
 import cors from "cors"
-config({ path: "./config/config.env" });
+config({ path: "process.env" });
 
 export const app = express();
 
