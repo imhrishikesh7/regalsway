6537b6e53d085af31abb5bb30a6e83c5fb2347d7
imhrishikesh7 <123190220+imhrishikesh7@users.noreply.github.com>
2023-09-26 02:48:49 -1100

    Add files via upload

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { checkout, paymentVerification } from "../controllers/paymentController.js";
-
-
 const router = express.Router();
+
+
+router.get('/getKey', (req, res) => {
+  res.status(200).json({ key: process.env.RAZORPAY_API_KEY });
+});
 
 router.route("/checkout")
 .post(async (req, res, next) => {
