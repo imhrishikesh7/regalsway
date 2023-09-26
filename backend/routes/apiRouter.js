91b245949a66e06db9796462869bfab064e354b8
imhrishikesh7 <123190220+imhrishikesh7@users.noreply.github.com>
2023-09-26 04:08:40 -1100

    Update apiRouter.js

--- backend/routes/apiRouter.js
+++ backend/routes/apiRouter.js
@@ -4,5 +4,8 @@ import paymentRoute from "./paymentRoutes.js";
 const apiRouter = express.Router();
 
 apiRouter.use("/payment", paymentRoute);
+app.get("/payment/getKey", (req, res) =>{
+    res.status(200).json({ key: process.env.RAZORPAY_API_KEY})
+})
 
 export default apiRouter;
