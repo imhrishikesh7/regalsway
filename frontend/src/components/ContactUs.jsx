d173737dcd7823d99fe79aa489a16e7514be871c
imhrishikesh7 <123190220+imhrishikesh7@users.noreply.github.com>
2023-11-07 00:47:37 -1100

    Update ContactUs.jsx

--- frontend/src/components/ContactUs.jsx
+++ frontend/src/components/ContactUs.jsx
@@ -12,7 +12,7 @@ export default function ContactUs() {
             <h2 className="section-summary">Need information? Fill out the form and send us a message.</h2>
             <form
               id="cont"
-              action="https://formsubmit.co/info@researchelite.co.in" 
+              action="https://formsubmit.co/info.researchelite.in" 
               method="POST"
               className="contact-form"
             >
